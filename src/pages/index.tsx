979e5e7580e3861493b594e090919a8a415489ae
Guilherme Aguiar <guilherme.aguiar@bitfy.app.com>
2023-05-14 23:01:21 -0300

    product carousel

---
+++ src/pages/index.tsx
@@ -0,0 +1,62 @@
+import Image from 'next/image';
+import { HomeContainer, Product } from '../styles/pages/home';
+
+import { useKeenSlider } from 'keen-slider/react';
+
+import 'keen-slider/keen-slider.min.css';
+
+
+import camiseta1 from '../assets/1.png';
+import camiseta2 from '../assets/2.png';
+import camiseta3 from '../assets/3.png';
+
+
+export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48
+    },
+  });
+
+
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
+        <Image src={camiseta1} width={520} height={520} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta2} width={520} height={520} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} width={520} height={520} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product className='keen-slider__slide'>
+        <Image src={camiseta3} width={520} height={520} alt="" />
+
+        <footer>
+          <strong>Camiseta X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  )
+}
